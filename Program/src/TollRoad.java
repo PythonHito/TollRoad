65569422723113e1ee023d0d0e847305bc5f368c
Owner <brett00gamer@gmail.com>
2018-03-03 11:41:53 +0000

    Completed TollRoad unit test suite!

--- Program/src/TollRoad.java
+++ Program/src/TollRoad.java
@@ -39,12 +39,51 @@ public class TollRoad {
         }
     }
 
-    public String ToString(){
-        return "SOme bull";
+    public String toString(){
+        return "Money collected: " + moneyMade + " Customers: " + customers;
         //TODO: DO THIS PLEASE
     }
 
     public static void main(String[] args){
-        //TODO: THIS LATER, SUNDAY IS A DAY OF REST YOU BAFOON
+        //Initialise toll with two customers
+        Vehicle customer1Vehicle = new Car("C001 B01", "Hyundai", 4);
+        CustomerAccount customer1 = new CustomerAccount("Dave", "Gloat", 1000,
+                customer1Vehicle);
+        Vehicle customer2Vehicle = new Car("SUP B01", "Toyota", 6);
+        CustomerAccount customer2 = new CustomerAccount("Steve", "Sleve", 1500,
+                customer2Vehicle);
+
+        TollRoad toll = new TollRoad();
+        toll.addCustomer(customer1);
+        toll.addCustomer(customer2);
+
+        //Check that getCustomers returns a deep copy
+        HashMap<String, CustomerAccount> customers = toll.getCustomers();
+        customers.remove("C001 B01");
+        System.out.println(!customers.equals(toll.getCustomers()));
+
+        //Check chargeCustomer
+        try {
+            toll.chargeCustomer("C001 B01");
+            System.out.println(toll.getMoneyMade() == 500);
+            System.out.println(toll.findCustomer("C001 B01").getAccountBalance() == 500);
+
+            toll.chargeCustomer("SUP B01");
+            System.out.println(toll.getMoneyMade() == 1100);
+            System.out.println(toll.findCustomer("SUP B01").getAccountBalance() == 900);
+
+            //Check that InsufficientAccountBalanceException propagates appropiately
+            toll.chargeCustomer("C001 B01");
+            toll.chargeCustomer("C001 B01");
+
+            //Should reach this point!
+            System.out.println(false);
+
+        } catch (InsufficientAccountBalanceException e){
+            System.out.println(toll.findCustomer("C001 B01").getAccountBalance() == 0);
+        }
+
+        System.out.println(toll);
+
     }
 }
